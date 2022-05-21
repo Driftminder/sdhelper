b2be569228e3a851b37f402cc191c65b46313159
Driftminder <colombi.guillaume@outlook.fr>
2022-05-21 22:37:20 +0200

    - Mise en place de l'interface electron + bootstrap - Configuration de l'IPC - Création du premier widget (placeholder)

--- main.js
+++ main.js
@@ -24,6 +24,7 @@ function createWindow () {
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: true,
+      devTools: true,
       // Preload des header
       preload: path.join(__dirname, 'preload.js')
     },
